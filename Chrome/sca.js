38dc2ffe4dd960b619317bc3e7feefe353ad1390
ChiefKiff <44556417+ChiefKiff@users.noreply.github.com>
2019-01-02 17:22:39 +0200

    Revert "V2.0"
    This reverts commit ad75f89b9109a42bd7f617b12649833b9bfbaeb5.

--- Chrome/sca.js
+++ Chrome/sca.js
@@ -9,106 +9,87 @@ Website: https://tcno.co/
 View this project on GitHub: https://github.com/TcNobo/TcNo-Steam-Card-Assistant
 */
 
-var console_info = ["%c TcNo SteamCardAssistant %cby TechNobo (Wesley Pyburn) %chttps://tcno.co/ ", 'background: #222; color: white','background: #222; color: #bada55','background: #222; color: lightblue'];
+//Displays developer information in console :)
+var console_info = [" %cTcNo SteamCardAssistant %cby TechNobo (Wesley Pyburn) %chttps://tcno.co/ ", 'background: #222; color: white','background: #222; color: #bada55','background: #222; color: lightblue'];
 console.log.apply(console, console_info);
 
-if (window.location.href.includes("gamecards")){
-	var s = document.createElement('script');
-	s.src = chrome.extension.getURL('TcNoEmbed.js');
+//Verifies that the website is HTTPS
+CheckIfHTTPS();
+//Checks to see if is a "gamecards" page or a "badges" page, and acts accordingly.
+if (window.location.href.includes( "gamecards" )) {
+	var s = document.createElement( 'script' );
+	s.src = chrome.extension.getURL( 'TcNoEmbed.js' );
 	s.onload = function() {
 		this.remove();
 	};
 	(document.head || document.documentElement).appendChild(s);
 	IndividualPage();
 }
-if (window.location.href.includes("badges")){
-	var s = document.createElement('script');
-	s.src = chrome.extension.getURL('TcNoEmbed.js');
+if (window.location.href.includes( "badges" )) {
+	var s = document.createElement( 'script' );
+	s.src = chrome.extension.getURL( 'TcNoEmbed.js' );
 	s.onload = function() {
 		this.remove();
 	};
 	(document.head || document.documentElement).appendChild(s);
 	BadgeListPage();
 }
-if (window.location.href.includes("multibuy")){
-	var s = document.createElement('script');
-	s.src = chrome.extension.getURL('TcNoEmbed.js');
-	s.onload = function() {
-		this.remove();
-	};
-	(document.head || document.documentElement).appendChild(s);
-	MultiBuyPage();
-}
 
-function MultiBuyPage() {
-	var tSSA = jQuery('#market_multi_accept_ssa').prop('checked');
-	var originalPriceString = jQuery('#market_multibuy_order_total')[0].innerHTML
-	var priceOfOne = Number(originalPriceString .replace(/[^0-9\.]+/g,""))
-	var priceOfFive = priceOfOne*5;
-	var afterPrice = originalPriceString.replace(priceOfOne, priceOfFive)
-	console.log('Original price: ' + originalPriceString)
-	console.log('Price of FIVE: ' + afterPrice)
-
-	//Add the button
-	jQuery('.market_multibuy h2').after('<a id="tcnoBuy5" class="btn_green_white_innerfade btn_medium_wide btn_uppercase market_unstyled_button" style="float: right; text-align: right; display: inline-block;"><span>Buy 5 of each (' + afterPrice + ')</span></a>')
-	//Keep the Steam Subscriber Agreement as it was before init. We won't accept it on your behalf, it's your job to do that.
-	jQuery('#market_multi_accept_ssa').prop('checked', tSSA)
-	//Change all values to 5
-	jQuery('#tcnoBuy5').click( function(e) {
-		if (!jQuery('#market_multi_accept_ssa').prop('checked')){
-			alert('Accept the "Steam Subscriber Agreement" at the bottom of the page first to use this button!')
-		}else{
-			e.preventDefault();
-			jQuery.each(
-				jQuery("input.market_multi_quantity"), function(){
-					jQuery(this).val('5');
-				});
-			jQuery('#market_multibuy_purchase').click();
-		}
-	});
+function CheckIfHTTPS() {
+	//Required for AJAX crafting within steamcommunity.com. One can not be HTTP and the other HTTPS.
+	if(window.location.href.startsWith( "http://" )) {
+		console.log( "The website is currently in http://. It is A. Insecure and B. Incompatible with TcNo Steam Card Assistant." );
+		var httpsLink = window.location.href.replace( "http://", "https://" );
+		window.location.href = httpsLink;
+	}
+	if(window.location.href.startsWith( "steamcommunity.com" )) {
+		console.log( "The website is currently in http://. It is A. Insecure and B. Incompatible with TcNo Steam Card Assistant." );
+		var httpsLink = window.location.href.replace( "steamcommunity.com", "https://steamcommunity.com" );
+		window.location.href = httpsLink;
+	}
 }
 
 function IndividualPage() {
 	//COMPLETE FOR INDIVIDUAL PAGES
 	//add to steamcommunity.com/id/*xyz*/gamecards/*381140*/
-	if (jQuery('div.badge_craft_button').length) {
+	if (jQuery( 'div.badge_craft_button' ).length) {
 		//define outerHTML function
-		jQuery.fn.outerHTML = function(){return (!this.length) ? this : (this[0].outerHTML || (function(el){var div = document.createElement('div');div.appendChild(el.cloneNode(true));var contents = div.innerHTML;div = null;return contents;})(this[0]));}
+		jQuery.fn.outerHTML = function() {return (!this.length) ? this : (this[0].outerHTML || (function(el) {var div = document.createElement( 'div' );div.appendChild(el.cloneNode(true));var contents = div.innerHTML;div = null;return contents;} )(this[0]));}
 		
-		if (jQuery('div.badge_info_description').length) {
-			var oldButton = jQuery("div.badge_craft_button").outerHTML();
-			jQuery("div.gamecard_badge_craftbtn_ctn").html(jQuery("div.gamecard_badge_craftbtn_ctn").html().replace(oldButton, "<br>" + oldButton));
-			jQuery("div.badge_craft_button").css({ "margin-top": "10px"});
-			jQuery("div.badge_content").css({ "height": "165px"});
-			jQuery("div.badge_info").css({ "min-width": "328px"});
-			jQuery("div.badge_current").css({ "margin-top": "41px"});
+		if (jQuery( 'div.badge_info_description' ).length) {
+		var oldButton = jQuery( "div.badge_craft_button" ).outerHTML();
+		jQuery( "div.gamecard_badge_craftbtn_ctn" ).html(jQuery( "div.gamecard_badge_craftbtn_ctn" ).html().replace(oldButton, "<br>" + oldButton));
+		jQuery( "div.badge_craft_button" ).css({ "margin-top": "10px" } );
+		jQuery( "div.badge_content" ).css({ "height": "165px" } );
+		jQuery( "div.badge_info" ).css({ "min-width": "328px" } );
+		jQuery( "div.badge_current" ).css({ "margin-top": "41px" } );
 		}
 		
-		var buttonsHTML = jQuery("div.gamecard_badge_craftbtn_ctn").html();
-		var craftLink = jQuery("div.badge_craft_button").attr("onclick");
-		var craftTimes = jQuery("div.badge_card_set_text_qty").html().substring(1,2);
-		jQuery("div.badge_card_set_text_qty").each(function() {
+		var buttonsHTML = jQuery( "div.gamecard_badge_craftbtn_ctn" ).html();
+		var craftLink = jQuery( "div.badge_craft_button" ).attr( "onclick" );
+		var craftTimes = jQuery( "div.badge_card_set_text_qty" ).html().substring(1,2);
+		jQuery( "div.badge_card_set_text_qty" ).each(function() {
 			var tempVal = jQuery(this).html().substring(1,2);
-			if (tempVal < craftTimes){
+			if (tempVal < craftTimes) {
 				craftTimes = tempVal;
 			}
-		});
+		} );
 		
 		var newCraftLink = "";
 		for (i = 0; i < craftTimes; i++) { 
 			newCraftLink += craftLink;
 		}
-		var newButtonHTML = jQuery("div.badge_craft_button").outerHTML().replace(craftLink, newCraftLink).replace(jQuery("div.badge_craft_button").html(), "Craft " + craftTimes + " Badge(s)");
-		newButtonHTML = newButtonHTML.replace('<div class="', '<style type="text/css">.TcNoButton {background-color:green;border: 1px solid lightgreen;margin-left:5px;} .TcNoButton:hover {background-color:#00b300;border-color:green;}</style><div class="TcNoButton ');
-		var buttonHTMLIncluded = buttonsHTML.replace('<div style="clear: left;"></div>',newButtonHTML + '<div style="clear: left;"></div>');
-		jQuery("div.gamecard_badge_craftbtn_ctn").html(buttonHTMLIncluded);
-		console.log("Multiple card button added!");
-	}else{console.log("Multiple card button NOT added!");}
+		var newButtonHTML = jQuery( "div.badge_craft_button" ).outerHTML().replace(craftLink, newCraftLink).replace(jQuery( "div.badge_craft_button" ).html(), "Craft " + craftTimes + " Badge(s)" );
+		newButtonHTML = newButtonHTML.replace( '<div class="', '<style type="text/css">.TcNoButton {background-color:green;border: 1px solid lightgreen;margin-left:5px;} .TcNoButton:hover {background-color:#00b300;border-color:green;}</style><div class="TcNoButton ' );
+		var buttonHTMLIncluded = buttonsHTML.replace( '<div style="clear: left;"></div>',newButtonHTML + '<div style="clear: left;"></div>' );
+		jQuery( "div.gamecard_badge_craftbtn_ctn" ).html(buttonHTMLIncluded);
+		console.log( "Multiple card button added!" );
+	}else{console.log( "Multiple card button NOT added!" );}
 	
 	//Open all cards in new tab button:
-	var textLocation = jQuery('div.badge_card_to_collect_header').parent();
-	jQuery('div.badge_card_to_collect_header').parent().html('<a href="javascript:OpenAllMarket()" style="display: inline-block;margin-left:10px" class="btn_grey_grey btn_medium"><span>Open all market links</span></a>' + jQuery('div.badge_card_to_collect_header').parent().html());
-	jQuery('div.badge_card_to_collect_header').css({ "display": "inline-block"});
+	var textLocation = jQuery( 'div.badge_card_to_collect_header' ).parent();
+	jQuery( 'div.badge_card_to_collect_header' ).parent().html( '<a href="javascript:OpenAllMarket()" style="display: inline-block;margin-left:10px" class="btn_grey_grey btn_medium"><span>Open all market links</span></a>' + jQuery( 'div.badge_card_to_collect_header' ).parent().html());
+	jQuery( 'div.badge_card_to_collect_header' ).css({ "display": "inline-block" } );
 }
 
 //used on the badge list page
@@ -116,24 +97,28 @@ function BadgeListPage() {
 	//Cards on the /badges/ page:
 	//Remove one of the 5 cards (to make space)
 	//OuterHTML ability
-	jQuery.fn.outerHTML = function(){return (!this.length) ? this : (this[0].outerHTML || (function(el){var div = document.createElement('div');div.appendChild(el.cloneNode(true));var contents = div.innerHTML;div = null;return contents;})(this[0]));}
+	jQuery.fn.outerHTML = function() {return (!this.length) ? this : (this[0].outerHTML || (function(el) {var div = document.createElement( 'div' );div.appendChild(el.cloneNode(true));var contents = div.innerHTML;div = null;return contents;} )(this[0]));}
 	//for each badge_cards, adds another DIV to get rid of the last card
-	jQuery("div.badge_cards").each(function() {
-		jQuery(this).html(jQuery(this).outerHTML().replace('<div','<div style="max-width:400px;overflow:hidden;"><div style="min-width:526px;"') + '</div>');
-	});
-	
-	jQuery("div.badge_progress_info").each(function() {
-		if (jQuery(this).find("a[href]").attr('href')){
-			var cardLink = jQuery(this).find("a[href]").attr('href');
-			var oldButton = jQuery(this).html();
-			var newButton = oldButton.replace(cardLink,"javascript:TcNoMulticraft('" + cardLink + "')").replace(jQuery(oldButton).html(), "Multiple");
-			newButton = newButton.replace('class="badge_craft_button"','')
-			newButton = newButton.replace('<a ', '<a class="badge_craft_button TcNoButton" ');
-			newButton = '<style type="text/css">.TcNoButton {background-color:green;border: 1px solid lightgreen;margin-left:5px;} .TcNoButton:hover {background-color:#00b300;border-color:green;}</style>' + newButton;
-			newButton = newButton.replace('href', 'style="min-width: 92px;display:inline-block;" href');
-			oldButton = oldButton.replace('href', 'style="min-width: 92px;max-width: 92px;display:inline-block;" href');
-			jQuery(this).html(newButton + oldButton);
+	jQuery( "div.badge_content" ).each(function() {
+		if (jQuery( this ).children( 'div.badge_progress_info' ).find( "a[href]" ).length){
+			jQuery( this ).children( 'div.badge_cards' ).html(jQuery(this).children( 'div.badge_cards' ).outerHTML().replace( '<div','<div style="max-width:400px;overflow:hidden;"><div style="min-width:526px;"' ) + '</div>' );
 		}
-		jQuery(this).css({ "min-width": "210px"});
-	});
+	} );
+	
+	jQuery( "div.badge_progress_info" ).each(function() {
+		if (jQuery(this).find( "a[href]" ).attr( 'href' )) {
+			var cardLink = jQuery(this).find( "a[href]" ).attr( 'href' );
+			var oldButton = jQuery(this).html();
+			var newButton = oldButton.replace(cardLink,"javascript:TcNoMulticraft( '" + cardLink + "' )" ).replace(jQuery(oldButton).html(), "Multiple" );
+			newButton = newButton.replace( 'class="badge_craft_button"','' )
+			newButton = newButton.replace( '<a ', '<a class="badge_craft_button TcNoButton" ' );
+			newButton = '<style type="text/css">.TcNoButton {background-color:green;border: 1px solid lightgreen;margin-left:5px;} .TcNoButton:hover {background-color:#00b300;border-color:green;}</style>' + newButton;
+			newButton = newButton.replace( 'href', 'style="min-width: 92px;display:inline-block;" href' );
+			oldButton = oldButton.replace( 'href', 'style="min-width: 92px;max-width: 92px;display:inline-block;" href' );
+			jQuery(this).html(newButton + oldButton);
+		jQuery(this).css({ "min-width": "210px" } );
+		}
+	} );
 }
+
+//outerHTML function adapted from https://gist.github.com/davemo/915570/0c848ef50e7d4aafb9bece3e8a90d50fd59716b3 (By: jboesch)
